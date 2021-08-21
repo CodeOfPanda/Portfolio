46058b01070d11aef66edf66ecd72e4789883608
CodeOfPanda <#Thailand052218>
2021-08-20 20:40:52 -0500

    meta updates

--- components/Container.js
+++ components/Container.js
@@ -18,7 +18,6 @@ export default function Container(props) {
   const meta = {
     title: 'Maggie Leigland',
     description: `Software engineering student, lifetime learner, nature lover`,
-    image: 'https://leerob.io/static/images/banner.png',
     type: 'website',
     ...customMeta
   };
@@ -29,15 +28,20 @@ export default function Container(props) {
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
         <meta content={meta.description} name="description" />
-        <meta property="og:url" content={`https://leerob.io${router.asPath}`} />
-        <link rel="canonical" href={`https://leerob.io${router.asPath}`} />
+        <meta
+          property="og:url"
+          content={`https://maggieleigland.me${router.asPath}`}
+        />
+        <link
+          rel="canonical"
+          href={`https://maggieleigland.me${router.asPath}`}
+        />
         <meta property="og:type" content={meta.type} />
-        <meta property="og:site_name" content="Lee Robinson" />
+        <meta property="og:site_name" content="Maggie Leigland" />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@leeerob" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
