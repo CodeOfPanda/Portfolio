3a9e2be856abd7e4d00d27d9b91cdffb034de94d
CodeOfPanda <#Thailand052218>
2021-11-01 13:20:14 -0600

    added profile picture

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,36 @@
 import Link from 'next/link';
 import Container from '@/components/Container';
+import Image from 'next/image';
+import profilePic from '../public/static/images/profile.png';
 
 export default function About() {
+  // const imgstyle = {
+  //   width: '400px',
+  //   height: 'auto',
+  //   borderRadius: '50%'
+  // };
   return (
     <Container title="About – Maggie Leigland">
-      <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
+      <div className="flex flex-col justify-center items-center max-w-2xl mx-auto mb-16">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
           About Me
         </h1>
+
         <div className="mb-8 prose leading-6 text-gray-600 dark:text-gray-400">
+          <div className="flex justify-center">
+            <Image
+              src={profilePic}
+              className="profile content-center"
+              alt="Picture of me!"
+              height="200"
+              width="200"
+            />
+          </div>
+          <style jsx global>{`
+            .profile {
+              border-radius: 50%;
+            }
+          `}</style>
           <p>
             Hi! I’m Maggie and I'm a computer science student with the goal of
             working on aerospace software!
